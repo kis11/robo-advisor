bc570e841b3fb38f7ce4d0f78f37cabd39703744
kis11 <59667818+kis11@users.noreply.github.com>
2020-02-19 02:51:32 -0500

    with recent highs and recent lows plus an input

--- roboadvisor.py
+++ roboadvisor.py
@@ -3,6 +3,10 @@
 import requests
 import json
 
+def to_usd(my_price):
+  return f"${my_price:,.2f}"
+
+symbol = input("Please enter a valid stock or crypto ticker here. Then hit enter.")
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
@@ -12,20 +16,41 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
-last_refreshed = ((parsed_response["Meta Data"]["3. Last Refreshed"]))
+tsd = parsed_response["Time Series (Daily)"]
+dates = list(tsd.keys())
+latest_day = dates[0]
+md = parsed_response["Meta Data"]
+last_refreshed = md["3. Last Refreshed"]
+latest_close = tsd[latest_day]["4. close"]
+
+high_prices = []
+low_prices = []
+
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    low_price = tsd[date]["3. low"]
+    high_prices.append(float(high_price))
+    low_prices.append(float(low_price))
+
+
+recent_high = max(high_prices)
+recent_low = min(low_prices)
+
+
+
 
 
 
 #print("-------------------------")
-#print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL:" + " " + symbol)
 #print("-------------------------")
 #print("REQUESTING STOCK MARKET DATA...")
-#print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT:" + "  " + last_refreshed)
 #print("-------------------------")
 #print("LATEST DAY: 2018-02-20") #has to be a variable
-#print("LATEST CLOSE: $100,000.00") #variable
-#print("RECENT HIGH: $101,000.00") #variable
-#print("RECENT LOW: $99,000.00") #variable
+print("LATEST CLOSE:" + "  " + to_usd(float(latest_close))) #variable
+print("RECENT HIGH:" + "  " + to_usd(float(recent_high))) #variable
+print("RECENT LOW:" + "  " + to_usd(float(recent_low))) #variable
 #print("-------------------------")
 #print("RECOMMENDATION: BUY!") #if < something, buy, if not , sell
 #print("RECOMMENDATION REASON: TODO")
